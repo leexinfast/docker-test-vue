fcf04880e8e22ffe753194f2f234850ff4919e09
leexinfast <leexinfast@foxmail.com>
2022-03-23 22:09:58 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-DOCKER_IMAGE=registry.cn-hangzhou.aliyuncs.com/leexinfast/docker-test-vue:latest
-DOCKER_REGISTRY=registry.cn-hangzhou.aliyuncs.com
-DOCKER_REGISTRY_USERNAME=leeyasuo
-DOCKER_REGISTRY_PASSWORD=716leexin.
+# DOCKER_IMAGE=registry.cn-hangzhou.aliyuncs.com/leexinfast/docker-test-vue:latest
+# DOCKER_REGISTRY=registry.cn-hangzhou.aliyuncs.com
+# DOCKER_REGISTRY_USERNAME=leeyasuo
+# DOCKER_REGISTRY_PASSWORD=716leexin.
+DOCKER_CONTAINER=docker-test-vue
+DOCKER_IMAGE=docker-test-vue
 
 yarn build
 docker build . -t ${DOCKER_IMAGE}
-docker login ${DOCKER_REGISTRY} -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD}
-docker push ${DOCKER_IMAGE}
+# docker login ${DOCKER_REGISTRY} -u ${DOCKER_REGISTRY_USERNAME} -p ${DOCKER_REGISTRY_PASSWORD}
+# docker push ${DOCKER_IMAGE}
+docker run -d --restart=always -p 10000:80 --name=${DOCKER_CONTAINER} ${DOCKER_IMAGE}
